df31011d0fd68dbcffee197df81e3e74a332fccf
Riad Younes <riad.younes@hotmail.com>
2025-11-26 18:07:54 -0300

    feat: add getWebhook route to API for fetching webhook details by ID

--- api/src/server.ts
+++ api/src/server.ts
@@ -10,6 +10,7 @@ import { fastifyCors } from '@fastify/cors'
 import ScalarApiReference from '@scalar/fastify-api-reference'
 import { listWebhooks } from './routes/list-webhooks'
 import { env } from './env'
+import { getWebhook } from './routes/get-webhook'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -38,7 +39,7 @@ app.register(ScalarApiReference, {
 })
 
 app.register(listWebhooks)
-
+app.register(getWebhook)
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log(`🔥 HTTP server running on http://localhost:3333!`)
   console.log(`📚 Docs available at http://localhost:3333/docs`)
